a5972a479110a7861bfce072f69b8910e9a21ae1
sorintolocica <sorinsagara@gmail.com>
2022-03-30 15:29:19 +0300

    v1

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+<head>git
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;1,200&family=Titillium+Web:wght@300&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title>Doramy.ro</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark-as-box">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">AnimeAZU</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Acasa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Anime Online</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Movie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Echipa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Despre noi</a>
+                </li>
+            </ul>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Cauta</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="alert alert-warning" role="alert">
+        Încă nu ești înregistrat? Loghează-te acum!
+    </div>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://i.pinimg.com/originals/f5/5a/de/f55ade672abd25177513caf9377c39dd.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+   <h4><span class="badge rounded-pill bg-primary">Episoade adaugate</span></h4>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card">
+                <img src="https://cdn.animetide.com/wp-content/uploads/high-school-dxd-season-5.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">High school DxD - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://i.pinimg.com/originals/f5/5a/de/f55ade672abd25177513caf9377c39dd.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Guilty Crown - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://static.hidive.com/news/2019/10/assassins-pride-simulcast-hidive-836x470.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Assasin Pride - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/91AzrSGuc6L._RI_.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Hundred - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://i0.wp.com/www.animegeek.com/wp-content/uploads/2021/07/How-a-Realist-Hero-Rebuilt-the-Kingdom-Season-2-release-date-Genjitsu-Shugi-Yuusha-no-Oukoku-Saikenki-Season-2.jpg?fit=1200%2C675&ssl=1" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Genjitsu Shugi Yuusha no Oukoku Saikenki - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="https://thesportsgrail.com/wp-content/uploads/2022/02/Attack-on-Titan-Episode-83-stock-1-min.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Attack On Titan - Episodul 01</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <ul class="pagination justify-content-center">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Pagina anterioara</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Pagina urmatoare</a></li>
+        </ul>
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
